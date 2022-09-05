30b8e55483469ee34fa07472498e80eac6149362
Michal Madej <mkmadej@interia.pl>
2022-09-05 21:21:18 +0200

    Improved showing heroes stats after the game

--- src/Main.java
+++ src/Main.java
@@ -6,45 +6,41 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Hero> semiFinals = new ArrayList<>();
-        List<Hero> fiNal = new ArrayList<>();
+        List<Hero> heroesSemiFinals = new ArrayList<>();
+        List<Hero> heroesfiNal = new ArrayList<>();
 
         for(int i=0; i<4; i++) {
             Game game = new Game();
             Hero hero = game.startGameRound();
-            semiFinals.add(hero);
+            heroesSemiFinals.add(hero);
         }
 
         System.out.println("SEMIFINALS");
-        for(Hero h : semiFinals) {
+        for(Hero h : heroesSemiFinals) {
             System.out.println(h);
             System.out.println();
         }
 
         for(int i=0; i<2; i++) {
-            Game game = new Game(semiFinals);
+            Game game = new Game(heroesSemiFinals);
             Hero hero = game.startGameFinal();
-            fiNal.add(hero);
+            heroesfiNal.add(hero);
         }
 
         System.out.println("FINAL");
-        for(Hero h : fiNal) {
+        for(Hero h : heroesfiNal) {
             System.out.println(h);
             System.out.println();
         }
 
-        Game game = new Game(fiNal);
+        Game game = new Game(heroesfiNal);
         Hero hero = game.startGameFinal();
-        //get winner to stats
-
-        System.out.println("ALL TIME WINNER IS:");
-        System.out.println(hero);
-
-        Game.stats.showAlliedCreatures(hero);
-        Game.stats.showAllHeroes();
 
         Game.stats.showWinner();
+        Game.stats.showAlliedCreatures(hero);
 
+        Game.stats.showWinnersOnArenas();
 
+     //   Game.stats.showAllHeroes();
     }
 }
--- src/Statistics.java
+++ src/Statistics.java
@@ -7,21 +7,18 @@ import java.util.List;
 public class Statistics {
     private final List<Arena> arenas = new ArrayList<>();
 
-    public List<Arena> getArenas() {
-        return arenas;
-    }
-
     public void showWinner() {
         System.out.println("Game winner:");
         System.out.println(arenas.get(arenas.size()-1).getWinner());
         System.out.println();
     }
 
+    //Shows heroes without winners on each of arenas
     public void showAllHeroes() {
         System.out.println("Other contestants:");
 
         for(Arena a : arenas) {
-            for(LifeForm h : a.getHeroes()) {
+            for(LifeForm h : a.getDeadHeroes()) {
                 if (!h.equals(a.getWinner())) System.out.println(h);
                 System.out.println();
             }
@@ -32,6 +29,21 @@ public class Statistics {
           System.out.println("Allied creatures: ");
           for(LifeForm lf : hero.getAlliedCreatures()) System.out.println(lf);
           System.out.println();
+    }
+
+    public void showWinnersOnArenas() {
+        int number = 0;
+        System.out.println("Winners on arenas: ");
+        for(Arena a : arenas) {
+            for(LifeForm h : a.getHeroes()) {
+                if (h.equals(a.getWinner())) {
+                    System.out.println("Arena #" + ++number);
+                    System.out.println(h);
+                    System.out.println();
+                }
+            }
+        }
+        System.out.println();
     }
 
     public void addArena(Arena arena) {
